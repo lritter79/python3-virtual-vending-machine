9228b07258cbe428eee39b66315381b4f5d2fbf0
Levon <levong2112@gmail.com>
2024-01-08 16:26:34 -0500

    revise setup script

--- data/db_setup.sql
+++ data/db_setup.sql
@@ -1,20 +1,50 @@
--- CREATE SEQUENCE catergories_serial AS integer START 1 OWNED BY item_categories.id;
+---- Table: public.item_categories
 
--- ALTER TABLE item_categories ALTER COLUMN id SET DEFAULT nextval('catergories_serial');
+-- DROP TABLE IF EXISTS public.item_categories;
 
--- INSERT INTO item_categories (name)
--- VALUES ('candy');
--- INSERT INTO item_categories (name)
--- VALUES ('gum');
--- INSERT INTO item_categories (name)
--- VALUES ('beverage');
--- INSERT INTO item_categories (name)
--- VALUES ('snack');
+-- CREATE TABLE IF NOT EXISTS public.item_categories
+-- (
+--     name text COLLATE pg_catalog."default",
+--     id integer NOT NULL DEFAULT 'nextval('catergories_serial'::regclass)',
+--     CONSTRAINT item_categories_pkey PRIMARY KEY (id)
+-- )
 
--- SELECT * from item_categories
--- CREATE SEQUENCE items_serial AS integer START 1 OWNED BY items.id;
+-- TABLESPACE pg_default;
 
--- ALTER TABLE items ALTER COLUMN id SET DEFAULT nextval('items_serial');
+-- ALTER TABLE IF EXISTS public.item_categories
+--     OWNER to postgres;
+
+--SELECT * from item_categories
+-- Table: public.items
+
+-- DROP TABLE IF EXISTS public.items;
+
+-- CREATE TABLE IF NOT EXISTS public.items
+-- (
+--     name text COLLATE pg_catalog."default",
+--     id integer NOT NULL DEFAULT 'nextval('items_serial'::regclass)',
+--     category_id integer NOT NULL,
+--     price real,
+--     CONSTRAINT items_pkey PRIMARY KEY (id),
+--     CONSTRAINT category_id_fkey FOREIGN KEY (category_id)
+--         REFERENCES public.item_categories (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE NO ACTION
+--         NOT VALID
+-- )
+
+-- TABLESPACE pg_default;
+
+-- ALTER TABLE IF EXISTS public.items
+--     OWNER to postgres;
+-- -- Index: fki_category_id_fkey
+
+-- -- DROP INDEX IF EXISTS public.fki_category_id_fkey;
+
+-- CREATE INDEX IF NOT EXISTS fki_category_id_fkey
+--     ON public.items USING btree
+--     (category_id ASC NULLS LAST)
+--     TABLESPACE pg_default;
 
 -- INSERT INTO items (name, category_id)
 -- VALUES ('Peanutbutter Cup', 1)
@@ -26,7 +56,27 @@
 -- VALUES ('Peanutbutter Sandwich', 4);
 -- INSERT INTO items (name, category_id)
 -- VALUES ('Yoohoo', 3);
+-- SELECT * FROM items
+-- Table: public.slots
 
+-- DROP TABLE IF EXISTS public.slots;
+
+-- CREATE TABLE IF NOT EXISTS public.slots
+-- (
+--     slot_row "char" NOT NULL,
+--     slot_column integer NOT NULL,
+--     item_id integer NOT NULL,
+--     CONSTRAINT unique_item UNIQUE (item_id),
+--     CONSTRAINT items_fkey FOREIGN KEY (item_id)
+--         REFERENCES public.items (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE NO ACTION
+-- )
+
+-- TABLESPACE pg_default;
+
+-- ALTER TABLE IF EXISTS public.slots
+--     OWNER to postgres;
 -- INSERT INTO slots (slot_row, slot_column, item_id)
 -- VALUES ('A', 2, 1);
 -- INSERT INTO slots (slot_row, slot_column, item_id)
@@ -39,18 +89,40 @@
 -- VALUES ('C', 7, 5);
 -- SELECT  * FROM slots
 
+
+---- Table: public.quantities
+
+-- DROP TABLE IF EXISTS public.quantities;
+
+-- CREATE TABLE IF NOT EXISTS public.quantities
+-- (
+--     quantity integer NOT NULL DEFAULT 0,
+--     item_id integer,
+--     CONSTRAINT uniq_item_id UNIQUE (item_id),
+--     CONSTRAINT items_fkey FOREIGN KEY (item_id)
+--         REFERENCES public.items (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE NO ACTION
+-- )
+
+-- TABLESPACE pg_default;
+
+-- ALTER TABLE IF EXISTS public.quantities
+--     OWNER to postgres;
+
+-- SELECT floor(random() * 10 + 1)::int;
 -- INSERT INTO quantities (quantity, item_id)
 -- VALUES ((SELECT floor(random() * 10 + 1)::int), 1);
--- INSERT INTO quantities (quantity, item_id)
--- VALUES ((SELECT floor(random() * 10 + 1)::int), 2);
--- INSERT INTO quantities (quantity, item_id)
+--INSERT INTO quantities (quantity, item_id)
+--VALUES ((SELECT floor(random() * 10 + 1)::int), 2);
+--INSERT INTO quantities (quantity, item_id)
 -- VALUES ((SELECT floor(random() * 10 + 1)::int), 3);
 -- INSERT INTO quantities (quantity, item_id)
 -- VALUES ((SELECT floor(random() * 10 + 1)::int), 4);
 -- INSERT INTO quantities (quantity, item_id)
 -- VALUES ((SELECT floor(random() * 10 + 1)::int), 5);
-
--- CREATE VIEW vending_machine AS
+--SELECT * FROM quantities
+--CREATE VIEW vending_machine AS
 -- SELECT items.id AS id, price, item_categories.name AS category_name 
 -- FROM items 
 -- JOIN item_categories ON items.category_id = item_categories.id;
@@ -80,3 +152,4 @@
 -- END;
 -- $$;
 
+-- CALL updateitemquantity(1)
